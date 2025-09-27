b5d8f448ace2c45063511b5306bd0c6a3249e2b8
Corey Ferguson <cpf716@gmail.com>
2025-09-27 00:58:17 -0400

    Initial Commit

---
+++ http/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  http
+//
+//  Created by Corey Ferguson on 9/27/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
